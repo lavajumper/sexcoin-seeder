fa213a0f54798d02f27a58daa67834f083d7f031
lavajumper <lavajumper@lavajumper.com>
2018-11-30 10:25:17 -0500

    add version.h

---
+++ version.h
@@ -0,0 +1,11 @@
+#ifndef _VERSION_H_
+#define _VERSION_H_ 1
+
+# define SEED_MAJOR_VERSION 0
+# define SEED_MINOR_VERSION 2
+# define SEED_BUILD_VERSION 1
+
+#include <cstring>
+extern const std::string seed_subver = "/sexcoin-seeder:0.02/";
+
+#endif
